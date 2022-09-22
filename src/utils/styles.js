28a6ff91a8bdca7774c82cc5cd1bcec9f7ccc5af
Marveeen <jmvroqueee@gmail.com>
2022-09-22 20:45:25 +0800

    Added styles utils

---
+++ src/utils/styles.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+export const PageContainer = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+
+export const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
